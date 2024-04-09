0deef586646bdab9abe3cc6109d3e80acc5ba488
Mannelz <emannuelm29@gmail.com>
2024-04-09 20:35:29 -0300

    color util

---
+++ condotech/lib/util/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Paleta {
+  static Color bgColor = const Color.fromRGBO(247, 252, 255, 1);
+  static Color azulClaro = const Color.fromRGBO(186, 192, 255, 1);
+  static Color azul = const Color.fromRGBO(0, 11, 128, 50);
+  static Color azulEscuro = const Color.fromRGBO(10, 15, 69, 27);
+  static Color cinzaClaro = const Color.fromRGBO(191, 191, 191, 75);
+  static Color cinza = const Color.fromRGBO(166, 166, 166, 65);
+  static Color dangerous = const Color.fromRGBO(234, 11, 11, 1);
+}
