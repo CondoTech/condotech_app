3e1b39f7834d5782400f4624761a82ad66b08da6
IgorLorentz <igorlorentz81@gmail.com>
2024-04-09 19:57:04 -0300

    main & register

---
+++ condotech/lib/pages/register.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatelessWidget 
+{
+  const Register({super.key});
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold
+    (
+      appBar: AppBar
+      (
+        title: const Text("Layout Modelo"),
+        centerTitle: true,
+      ),
+
+      body: const Center
+      (
+        child: Text("Comece aqui!"),
+      ),
+    );
+  }
+}
