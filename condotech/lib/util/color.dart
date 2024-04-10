1fd3165c43f011eef6a5226677ecca572ac749a4
IgorLorentz <igorlorentz81@gmail.com>
2024-04-09 21:53:51 -0300

    Update color

--- condotech/lib/util/color.dart
+++ condotech/lib/util/color.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Paleta {
   static Color bgColor = const Color.fromRGBO(247, 252, 255, 1);
   static Color lilasClaro = const Color.fromRGBO(186, 192, 255, 1);
-  static Color lilas = const Color.fromRGBO(148, 154, 227, 89);
+  static Color lilas = const Color.fromRGBO(148, 154, 227, 1);
   static Color azul = const Color.fromRGBO(0, 11, 128, 50);
   static Color azulEscuro = const Color.fromRGBO(10, 15, 69, 27);
   static Color cinzaClaro = const Color.fromRGBO(191, 191, 191, 75);
