da163009fc5f1edb68ec63680787a4d6e7d7d66c
IgorLorentz <igorlorentz81@gmail.com>
2024-04-09 21:53:34 -0300

    Update register

--- condotech/lib/pages/register.dart
+++ condotech/lib/pages/register.dart
@@ -1,23 +1,104 @@
+import 'package:condotech/util/color.dart';
 import 'package:flutter/material.dart';
 
-class Register extends StatelessWidget 
-{
+class Register extends StatelessWidget {
   const Register({super.key});
 
   @override
-  Widget build(BuildContext context) 
-  {
-    return Scaffold
-    (
-      appBar: AppBar
-      (
-        title: const Text("Layout Modelo"),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Paleta.bgColor,
+      appBar: AppBar(
+        backgroundColor: Paleta.azulEscuro,
+        title: const Text(style: TextStyle(color: Colors.white), "Login"),
         centerTitle: true,
       ),
-
-      body: const Center
-      (
-        child: Text("Comece aqui!"),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  color: Paleta.lilas),
+              width: 330,
+              height: 497,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Nome"),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                        color: Paleta.lilasClaro),
+                    width: 260.0,
+                    height: 38.0,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Text("CPF"),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                        color: Paleta.lilasClaro),
+                    width: 260.0,
+                    height: 38.0,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Text(textAlign: TextAlign.left, "Digite sua nova senha"),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                        color: Paleta.lilasClaro),
+                    width: 260.0,
+                    height: 38.0,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Text("Digite sua senha novamente"),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                        color: Paleta.lilasClaro),
+                    width: 260.0,
+                    height: 38.0,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Paleta.lilasClaro,
+                        textStyle: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(9.0),
+                            side: const BorderSide(
+                                color: Colors.black, width: 1)),
+                        foregroundColor: Colors.black,
+                      ),
+                      onPressed: () => print("clicou"),
+                      child: Text("Cadastrar"))
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
