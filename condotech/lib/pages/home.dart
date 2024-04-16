adcd67f6df183d12d5aebde91287119d77824316
IgorLorentz <igorlorentz81@gmail.com>
2024-04-16 20:57:41 -0300

    Update home

--- condotech/lib/pages/home.dart
+++ condotech/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:condotech/util/color.dart';
+import 'package:condotech/util/full_appbar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,28 +12,7 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Paleta.bgColor,
       appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-                color: Paleta.azulEscuro,
-                onPressed: () => print("VIU A NOTIFICAÇÃO"),
-                icon: Icon(Icons.notifications)),
-            Image.asset(
-              'assets/images/logo.png',
-              fit: BoxFit.contain,
-              height: 32,
-            ),
-            IconButton(
-                color: Paleta.azulEscuro,
-                onPressed: () => print("PROCUROU"),
-                icon: Icon(Icons.search)),
-            IconButton(
-                color: Paleta.azulEscuro,
-                onPressed: () => print("DESLOGOU"),
-                icon: Icon(Icons.logout))
-          ],
-        ),
+        title: FullAppBar(),
         centerTitle: true,
       ),
       body: Center(
@@ -52,53 +32,59 @@ class Home extends StatelessWidget {
                     ),
                     onPressed: () => print("CONTA DO USER"),
                     child: Icon(Icons.account_circle_outlined)),
-                Text(style: TextStyle(fontFamily: "Mukta"), "Olá, Usuário"),
+                Text(
+                    style: TextStyle(fontSize: 24, fontFamily: "Mukta"),
+                    "Olá, Usuário"),
               ],
             ),
             SizedBox(
               height: 23,
             ),
+
             //Dados condominio
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  height: 99,
-                  width: 182,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(9),
-                    color: Paleta.lilas,
-                  ),
-                  child: Row(
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                              style:
-                                  TextStyle(fontFamily: "Mukta", fontSize: 11),
-                              "Saldo do condomínio"),
-                          Text(
-                              style:
-                                  TextStyle(fontFamily: "Mukta", fontSize: 15),
-                              "R\$1000,00"),
-                          IconButton(
-                              color: Paleta.azulEscuro,
-                              onPressed: () => print("VIU A CONTA"),
-                              icon: Icon(Icons.remove_red_eye)),
-                          Container(
-                            height: 29,
-                            width: 182,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(9),
-                              color: Paleta.lilasClaro,
+            Container(
+              margin: EdgeInsets.only(left: 17),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 99,
+                    width: 182,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(9),
+                      color: Paleta.lilas,
+                    ),
+                    child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                                style: TextStyle(
+                                    fontFamily: "Mukta", fontSize: 11),
+                                "Saldo do condomínio"),
+                            Text(
+                                style: TextStyle(
+                                    fontFamily: "Mukta", fontSize: 15),
+                                "R\$1000,00"),
+                            IconButton(
+                                color: Paleta.azulEscuro,
+                                onPressed: () => print("VIU A CONTA"),
+                                icon: Icon(Icons.remove_red_eye)),
+                            Container(
+                              height: 29,
+                              width: 182,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(9),
+                                color: Paleta.lilasClaro,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
             SizedBox(
               height: 34,
@@ -237,6 +223,19 @@ class Home extends StatelessWidget {
                 )
               ],
             ),
+            SizedBox(
+              height: 39,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 45),
+              child: Row(
+                children: [
+                  Text(
+                      style: TextStyle(fontFamily: "Mukata", fontSize: 18),
+                      "Calendário")
+                ],
+              ),
+            )
           ],
         ),
       ),
