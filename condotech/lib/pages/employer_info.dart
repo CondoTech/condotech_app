338c9c81476a359787b0c7b0159c5596aee19fcd
Mannelz <emannuelm29@gmail.com>
2024-04-23 17:07:01 -0300

    create employer info

--- condotech/lib/pages/employer_info.dart
+++ condotech/lib/pages/employer_info.dart
@@ -1,3 +1,7 @@
+import 'package:condotech/util/appbar.dart';
+import 'package:condotech/util/color.dart';
+import 'package:condotech/util/footer.dart';
+import 'package:condotech/util/information_field.dart';
 import 'package:flutter/material.dart';
 
 class EmployerInfo extends StatelessWidget {
@@ -6,13 +10,39 @@ class EmployerInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Layout Modelo"),
-        centerTitle: true,
+      appBar: MinAppBar(),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(62, 50, 62, 35),
+              child: Center(
+                child: Column(
+                  children: [
+                    Center(
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundImage: AssetImage("assets/images/logo.png"),
+                      ),
+                    ),
+                    SizedBox(height: 25),
+                    InformationField(title: "Nome:", text: "employerName"),
+                    SizedBox(height: 25),
+                    InformationField(title: "CPF:", text: "employerCPF"),
+                    SizedBox(height: 25),
+                    InformationField(
+                        title: "Serviço:", text: "employerService"),
+                    SizedBox(height: 25),
+                    InformationField(
+                        title: "Responsável:", text: "employerResponsible"),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      body: const Center(
-        child: Text("Eu vou fazer! by Eyshila"),
-      ),
+      bottomNavigationBar: footer(),
     );
   }
 }
