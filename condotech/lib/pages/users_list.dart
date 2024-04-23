d519c35fbdc7caa40035f0c089b9806d461b5a80
IgorLorentz <igorlorentz81@gmail.com>
2024-04-23 18:29:38 -0300

    Update user list

--- condotech/lib/pages/users_list.dart
+++ condotech/lib/pages/users_list.dart
@@ -1,5 +1,6 @@
 import 'package:condotech/util/appbar.dart';
 import 'package:condotech/util/color.dart';
+import 'package:condotech/util/footer.dart';
 import 'package:condotech/util/text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -16,48 +17,56 @@ class UsersList extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            UserData("José", "123"),
-            UserData("José", "123"),
-            UserData("José", "123"),
-            UserData("José", "123"),
-            UserData("José", "123"),
-            UserData("José", "123"),
-            UserData("José", "123"),
-          ],
-        ),
+        child: ListView(children: [
+          userData("Cléber", "123"),
+          userData("Cléber", "123"),
+          userData("Cléber", "123"),
+          userData("Cléber", "123"),
+          userData("Cléber", "123"),
+          userData("Cléber", "123"),
+        ]),
       ),
+      bottomNavigationBar: footer(),
     );
   }
 
-  Row UserData(String name, String apto) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(color: Paleta.azulEscuro, size: 60, Icons.account_circle),
-        Container(
-          padding: EdgeInsets.only(left: 15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  smallTextBold("Nome: "),
-                  smallText(name),
-                ],
-              ),
-              Row(
-                children: [
-                  smallTextBold("Apto: "),
-                  smallText(apto),
-                ],
-              )
-            ],
-          ),
-        )
-      ],
+  ListTile userData(String name, String apto) {
+    return ListTile(
+      leading: Icon(color: Paleta.azulEscuro, size: 60, Icons.account_box),
+      title: smallText(name),
+      subtitle: smallText(apto),
+      trailing: Icon(Icons.navigate_next),
+      iconColor: Paleta.azulEscuro,
+      onTap: () => print("FOI PRO PERFIL"),
     );
   }
+
+  // Row UserData(String name, String apto) {
+  //   return Row(
+  //     mainAxisAlignment: MainAxisAlignment.center,
+  //     children: [
+  //       Icon(color: Paleta.azulEscuro, size: 60, Icons.account_circle),
+  //       Container(
+  //         padding: EdgeInsets.only(left: 15),
+  //         child: Column(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: [
+  //             Row(
+  //               children: [
+  //                 smallTextBold("Nome: "),
+  //                 smallText(name),
+  //               ],
+  //             ),
+  //             Row(
+  //               children: [
+  //                 smallTextBold("Apto: "),
+  //                 smallText(apto),
+  //               ],
+  //             )
+  //           ],
+  //         ),
+  //       )
+  //     ],
+  //   );
+  // }
 }
