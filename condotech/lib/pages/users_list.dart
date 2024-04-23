53bc93c98b97fee3d256441759e0a1b6c79e19dd
IgorLorentz <igorlorentz81@gmail.com>
2024-04-22 21:19:47 -0300

    Create UserList

--- condotech/lib/pages/users_list.dart
+++ condotech/lib/pages/users_list.dart
@@ -1,4 +1,8 @@
+import 'package:condotech/util/appbar.dart';
+import 'package:condotech/util/color.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class UsersList extends StatelessWidget {
   const UsersList({super.key});
@@ -7,12 +11,29 @@ class UsersList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Layout Modelo"),
+        title: MinAppBar(),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text("Comece aqui!"),
+      body: Center(
+        child: Column(
+          children: [
+            UserData("José", "123"),
+          ],
+        ),
       ),
+    );
+  }
+
+  Row UserData(String name, String apto) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(Icons.account_circle),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [Text(name), Text(apto)],
+        )
+      ],
     );
   }
 }
