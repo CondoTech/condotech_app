28da21fbaded19cde36dfa11bcfab52adfaf4754
IgorLorentz <igorlorentz81@gmail.com>
2024-06-05 18:43:58 -0300

    fix

--- condotech/lib/main.dart
+++ condotech/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:condotech/pages/cadastroUser.dart';
 import 'package:condotech/pages/home.dart';
 import 'package:flutter/material.dart';
 
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.blue,
       ),
-      home: const Home(), // Altere para a página que estiver mexendo
+      home: const CadastroUser(), // Altere para a página que estiver mexendo
     );
   }
 }
